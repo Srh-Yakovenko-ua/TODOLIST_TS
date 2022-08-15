4c0fe8ffeb8cf13d508492033e916b5424b2a36f
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-08-15 21:24:37 +0300

    add key in li

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-type TaskType = {
+export type TaskType = {
     id : number
     title : string
     isDone : boolean
@@ -20,9 +20,9 @@ export const TodoList = (props: TodoListPropsType) => {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                <li key={props.tasks[0].id}><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                <li key={props.tasks[1].id}><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                <li key={props.tasks[2].id}><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
             </ul>
             <div>
                 <button>All</button>
