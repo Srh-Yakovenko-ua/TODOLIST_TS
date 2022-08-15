c313179e444bbaac45e9088ef7b10879218368df
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-08-15 20:09:12 +0300

    changed the internal to a separate component and called it

---
+++ src/TodoList.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const TodoList = () => {
+    return (
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+}
+
