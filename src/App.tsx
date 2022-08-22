8ae17d19ac7741a10c253408b6959cf536b75baf
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-08-22 20:50:22 +0300

    add useState for removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,34 @@
 import React from 'react';
 import './App.css';
-import {TaskType, TodoList} from "./TodoList";
+import {TaskType, TodoList} from './TodoList';
+import {useState} from 'react';
 
 function App() {
-    // BLL : (уровень бизнесс логики)
-    const todoListTitle_1: string = 'What to learn today';
-    const todoListTitle_2: string = 'What to learn next week';
-    const tasks_1: Array<TaskType> = [
+
+    const todoListTitle: string = 'What to learn today';
+
+    const [tasks, setTasks] = useState<Array<TaskType>>([
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'JS&TS', isDone: true},
         {id: 3, title: 'REACT', isDone: false},
-    ];
+        {id: 4, title: 'ReactNative', isDone: false},
+    ]);
 
-    const tasks_2 : Array<TaskType> = [
-        {id: 4, title: '....', isDone: true},
-        {id: 5, title: '....', isDone: false},
-        {id: 6, title: '....', isDone: false},
-    ];
-
-
-
-
-    // UI:  (отображать)
+    const removeTask = (taskID: number) => {
+        setTasks(tasks.filter(t => t.id !== taskID)) //10ms
+       // console.log(tasks) // работает асинхронно
+    }
 
 
     return (
         <div className="App">
-            <TodoList title={todoListTitle_1} tasks={tasks_1}/>
-            {/*TodoList({title:'What to learn', tasks : {})*/}
-            <TodoList title={todoListTitle_2} tasks={tasks_2}/>
+            <TodoList title={todoListTitle}
+                      tasks={tasks}
+                      removeTask={removeTask}/>
         </div>
     );
 }
 
 export default App;
+
+
