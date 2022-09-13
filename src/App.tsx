b48302675d0dcca407c4a362f78a790c88b239dc
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-09-13 19:43:29 +0300

    add new array

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,44 @@ type TodolistsType = {
 
 function App() {
 
+    // let [todolists, setTodolists] = useState<Array<TodolistsType>>([
+    //     {id: v1(), title: 'What to learn', filter: 'active'},
+    //     {id: v1(), title: 'What to buy', filter: 'all'},
+    // ])
+
+    let todolistID1=v1();
+    let todolistID2=v1();
+
     let [todolists, setTodolists] = useState<Array<TodolistsType>>([
-        {id: v1(), title: 'What to learn', filter: 'active'},
-        {id: v1(), title: 'What to buy', filter: 'all'},
+        {id: todolistID1, title: 'What to learn', filter: 'all'},
+        {id: todolistID2, title: 'What to buy', filter: 'all'},
     ])
 
+    let [tasks, setTasks] = useState({
+        [todolistID1]:[
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistID2]:[
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+            {id: v1(), title: "ReactJS2", isDone: false},
+            {id: v1(), title: "Rest API2", isDone: false},
+            {id: v1(), title: "GraphQL2", isDone: false},
+        ]
+    });
 
-    let [tasks, setTasks] = useState([
-        {id: v1(), title: 'HTML&CSS', isDone: true},
-        {id: v1(), title: 'JS', isDone: true},
-        {id: v1(), title: 'ReactJS', isDone: false},
-        {id: v1(), title: 'Rest API', isDone: false},
-        {id: v1(), title: 'GraphQL', isDone: false},
-    ]);
+
+    // let [tasks, setTasks] = useState([
+    //     {id: v1(), title: 'HTML&CSS', isDone: true},
+    //     {id: v1(), title: 'JS', isDone: true},
+    //     {id: v1(), title: 'ReactJS', isDone: false},
+    //     {id: v1(), title: 'Rest API', isDone: false},
+    //     {id: v1(), title: 'GraphQL', isDone: false},
+    // ]);
 
     //  let [filter, setFilter] = useState<FilterValuesType>('all');
 
