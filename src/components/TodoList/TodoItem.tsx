8e01453978ef6b2b006e3b87ad3ca7f91a1acbf8
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-01-07 23:09:05 +0300

    - added user authorization check
     - markup into a separate variable

--- src/components/TodoList/TodoItem.tsx
+++ src/components/TodoList/TodoItem.tsx
@@ -1,58 +1,69 @@
-import React from 'react';
-import {useAppDispatch} from '../../store';
-import {createNewTaskThunk} from '../../store/tasks/tasks-reducer';
-import {removeTodoThunk, updateTodoTitleThunk} from '../../store/todolist/todolist-reducer';
-import {FiltersType} from '../../store/todolist/todolist-types';
-import {changeFilterTodoAC} from '../../store/todolist/todolist-action';
-import {AddItemForm} from '../../common/AddItemForm';
-import {TaskList} from '../Task/TaskList';
-import {RequestStatusType} from '../../store/app/app-reducer';
-import {TitleFormTodo} from './TitleFormTodo';
-import {ButtonGroupFilter} from './ButtonGroupFilter';
+import React from "react";
+import { useAppDispatch } from "../../store";
+import { createNewTaskTC } from "../../store/tasks/tasks-reducer";
+import {
+  removeTodoTC,
+  updateTodoTitleTC,
+} from "../../store/todolist/todolist-reducer";
+import { FiltersType } from "../../store/todolist/todolist-types";
+import { changeFilterTodoAC } from "../../store/todolist/todolist-action";
+import { TitleFormTodo } from "./TitleFormTodo/TitleFormTodo";
+import { AddItemForm } from "../../common/AddItemForm";
+import { TaskList } from "../Task/TaskList";
+import { ButtonGroupFilter } from "./BtnGroupFilter/ButtonGroupFilter";
+import { RequestStatusType } from "../../store/app/app-reducer";
+import styled from "styled-components";
+import { entityStatusDisabledUtils } from "../../utils/entity-status-disabled-utils";
 
-interface TodoListType {
-    todolistId: string
-    title: string
-    filter: FiltersType
-    entityTodoStatus: RequestStatusType
-}
+type TodoListType = {
+  todolistId: string;
+  title: string;
+  filter: FiltersType;
+  entityTodoStatus: RequestStatusType;
+};
 
+const AddItemFormWrapper = styled.div`
+  display: flex;
+  gap: 30px;
+`;
 
 export const TodoItem: React.FC<TodoListType> = ({
-                                                     todolistId,
-                                                     title,
-                                                     filter,
-                                                     entityTodoStatus
-                                                 }) => {
-    const dispatch = useAppDispatch();
+  todolistId,
+  title,
+  filter,
+  entityTodoStatus,
+}) => {
+  const dispatch = useAppDispatch();
 
-    const addTask = (title: string) => dispatch(createNewTaskThunk(title, todolistId))
-    const onChangeTodoTitle = (title: string) => dispatch(updateTodoTitleThunk(todolistId, title))
-    const removeTodo = () => dispatch(removeTodoThunk(todolistId))
+  const addTask = (title: string) =>
+    dispatch(createNewTaskTC(title, todolistId));
+  const onChangeTodoTitle = (title: string) =>
+    dispatch(updateTodoTitleTC(todolistId, title));
+  const removeTodo = () => dispatch(removeTodoTC(todolistId));
 
+  const filterTodo = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const DATA_SET = e.currentTarget.dataset.filter as FiltersType;
+    DATA_SET && dispatch(changeFilterTodoAC(DATA_SET, todolistId));
+  };
 
-    const filterTodo = (e: React.MouseEvent<HTMLButtonElement>) => {
-        const DATA_SET = e.currentTarget.dataset.filter as FiltersType
-        DATA_SET && dispatch(changeFilterTodoAC(DATA_SET, todolistId))
-    }
+  return (
+    <>
+      <TitleFormTodo
+        title={title}
+        entityTodoStatus={entityTodoStatus}
+        onChangeTodoTitle={onChangeTodoTitle}
+        removeTodo={removeTodo}
+      />
+      <AddItemFormWrapper>
+        <AddItemForm
+          addItem={addTask}
+          disabled={entityStatusDisabledUtils(entityTodoStatus)}
+        />
+      </AddItemFormWrapper>
+      <TaskList filter={filter} todolistId={todolistId} />
+      <ButtonGroupFilter filter={filter} filterTodo={filterTodo} />
+    </>
+  );
+};
 
-    return (
-        <>
-            <TitleFormTodo title={title}
-                           entityTodoStatus={entityTodoStatus}
-                           onChangeTodoTitle={onChangeTodoTitle}
-                           removeTodo={removeTodo}
-            />
-            <div style={{display: 'flex', gap: '30px'}}>
-                <AddItemForm addItem={addTask}
-                             disabled={entityTodoStatus === 'loading'}/>
-            </div>
-            <TaskList filter={filter}
-                      todolistId={todolistId}/>
-            <ButtonGroupFilter filter={filter}
-                               filterTodo={filterTodo}
-            />
-        </>
-    )
-}
 
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,33 +1,61 @@
-import React from 'react';
-import {useAppSelector} from '../../store';
-import {TodoStateType} from '../../store/todolist/todolist-types';
-import {allTodoSelectors} from '../../store/todolist/todolist-selectors';
-import Grid2 from '@mui/material/Unstable_Grid2';
-import {Paper} from '@mui/material';
-import {TodoItem} from './TodoItem';
-
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store";
+import { allTodoSelectors } from "../../store/todolist/todolist-selectors";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Container, Paper } from "@mui/material";
+import {TodoItem} from "./TodoItem";
+import { AddItemForm } from "../../common/AddItemForm";
+import {
+  createNewTodoTC,
+  getTodoListsTC,
+} from "../../store/todolist/todolist-reducer";
+import { appAuthSelectors } from "../../store/appAuth/appAuth-selectors";
+import { Navigate } from "react-router-dom";
 
 export const TodoList = () => {
-    const todo = useAppSelector<TodoStateType[]>(allTodoSelectors)
+  const dispatch = useAppDispatch();
+  const todo = useAppSelector(allTodoSelectors);
+  const isAuth = useAppSelector(appAuthSelectors);
 
+  const createNewTodo = (title: string) => dispatch(createNewTodoTC(title));
+
+  useEffect(() => {
+    if (!isAuth) return;
+    dispatch(getTodoListsTC());
+  }, [dispatch, isAuth]);
+
+  if (!isAuth) return <Navigate to={"/login"} />;
+
+  const todoItemLayer = todo.map((todo) => {
     return (
-        <Grid2 container spacing={3}>
-            {todo.map(todo => {
-                return (
-                    <Grid2 key={todo.id}>
-                        <Paper style={{padding: '15px'}} elevation={4}>
-                            <TodoItem
-                                key={todo.id}
-                                todolistId={todo.id}
-                                title={todo.title}
-                                filter={todo.filter}
-                                entityTodoStatus={todo.entityStatus}
-                            />
-                        </Paper>
-                    </Grid2>
-                )
-            })}
-        </Grid2>
-    )
-};
+      <Grid key={todo.id}>
+        <Paper style={{ padding: "15px" }} elevation={4}>
+          <TodoItem
+            key={todo.id}
+            todolistId={todo.id}
+            title={todo.title}
+            filter={todo.filter}
+            entityTodoStatus={todo.entityStatus}
+          />
+        </Paper>
+      </Grid>
+    );
+  });
 
+  return (
+    <div>
+      <Container fixed>
+        <Grid container style={{ padding: "15px" }}>
+          <AddItemForm
+            addItem={createNewTodo}
+            label="create new Todo"
+            title="add Todo"
+          />
+        </Grid>
+        <Grid container spacing={3}>
+          {todoItemLayer}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
