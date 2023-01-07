6cce4be4e1c44470821f07f82262b8152443f5b7
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-01-07 22:51:29 +0300

    Refactor Preloader
     - create a separate component and a wrapper styles for the preloader

---
+++ src/common/preloader/Preloader.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { LinearProgress } from "@mui/material";
+
+export const Preloader = () => {
+  return <LinearProgress color="primary" />;
+};
---
+++ src/common/preloader/preloader-style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const WrapperAppPreloader = styled.div`
+  position: fixed;
+  top: 30%;
+  text-align: center;
+  width: 100%;
+`;
