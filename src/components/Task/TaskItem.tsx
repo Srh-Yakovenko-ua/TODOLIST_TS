0282e3cbcb7d27f140fbdb3adb37b61761d6a76b
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-01-07 23:02:39 +0300

    refactor TaskItem and TaskList

--- src/components/Task/TaskItem.tsx
+++ src/components/Task/TaskItem.tsx
@@ -1,57 +1,59 @@
-import React, {ChangeEvent} from 'react';
-import {TaskStatuses} from '../../store/tasks/tasks-types';
-import {EditableSpan} from '../../common/EditableSpan';
-import {Checkbox, IconButton, Tooltip} from '@mui/material';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import {RequestStatusType} from '../../store/app/app-reducer';
-
+import React, { ChangeEvent } from "react";
+import { TaskStatuses } from "../../store/tasks/tasks-types";
+import { EditableSpan } from "../../common/EditableSpan/EditableSpan";
+import { Checkbox, IconButton, Tooltip } from "@mui/material";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { RequestStatusType } from "../../store/app/app-reducer";
+import { entityStatusDisabledUtils } from "../../utils/entity-status-disabled-utils";
 
 interface TaskTypeProps {
-    todolistId: string
-    taskTitle: string
+  todolistId: string;
+  taskTitle: string;
+  taskID: string;
+  taskStatus: TaskStatuses;
+  entityStatusTask: RequestStatusType;
+  updateStatusCheckbox: (
+    e: ChangeEvent<HTMLInputElement>,
     taskID: string
-    taskStatus: TaskStatuses
-    entityStatusTask: RequestStatusType
-    updateStatusCheckbox: (e: ChangeEvent<HTMLInputElement>, taskID: string) => void
-    handlerRemoveTask: (taskID: string) => void
-    onChangeValueTextSpan: (newValue: string, taskID: string) => void
+  ) => void;
+  handlerRemoveTask: (taskID: string) => void;
+  onChangeValueTextSpan: (newValue: string, taskID: string) => void;
 }
 
 export const TaskItem: React.FC<TaskTypeProps> = ({
-                                                      taskTitle,
-                                                      taskID,
-                                                      entityStatusTask,
-                                                      updateStatusCheckbox,
-                                                      handlerRemoveTask,
-                                                      onChangeValueTextSpan,
-                                                      taskStatus
-                                                  }) => {
-
-
-    return (
-        <div>
-            <Checkbox
-                disabled={entityStatusTask === 'loading'}
-                checked={taskStatus === TaskStatuses.Completed}
-                onChange={(e) => updateStatusCheckbox(e, taskID)}
-
-            />
-            <EditableSpan title={taskTitle}
-                          disabled={entityStatusTask === 'loading'}
-                          taskStatus={taskStatus}
-                          onChangeValueTextSpan={(newValue: string) => onChangeValueTextSpan(newValue, taskID)}
-            />
-            <Tooltip title="delete Task"
-                     placement="right">
-                <IconButton disabled={entityStatusTask === 'loading'}
-                            onClick={() => handlerRemoveTask(taskID)}
-                            color="primary"
-                >
-                    <DeleteForeverIcon/>
-                </IconButton>
-            </Tooltip>
-
-        </div>
-    );
+  taskTitle,
+  taskID,
+  entityStatusTask,
+  updateStatusCheckbox,
+  handlerRemoveTask,
+  onChangeValueTextSpan,
+  taskStatus,
+}) => {
+  const entityStatusDisabled = entityStatusDisabledUtils(entityStatusTask);
+  return (
+    <div>
+      <Checkbox
+        disabled={entityStatusDisabled}
+        checked={taskStatus === TaskStatuses.Completed}
+        onChange={(e) => updateStatusCheckbox(e, taskID)}
+      />
+      <EditableSpan
+        title={taskTitle}
+        disabled={entityStatusDisabled}
+        taskStatus={taskStatus}
+        onChangeValueTextSpan={(newValue: string) =>
+          onChangeValueTextSpan(newValue, taskID)
+        }
+      />
+      <Tooltip title="delete Task" placement="right">
+        <IconButton
+          disabled={entityStatusDisabled}
+          onClick={() => handlerRemoveTask(taskID)}
+          color="primary"
+        >
+          <DeleteForeverIcon />
+        </IconButton>
+      </Tooltip>
+    </div>
+  );
 };
-
--- src/components/Task/TaskList.tsx
+++ src/components/Task/TaskList.tsx
@@ -1,48 +1,54 @@
-import React, {ChangeEvent} from 'react';
-import {TaskItem} from './TaskItem';
-import {useAppDispatch, useAppSelector} from '../../store';
-import {TaskStatuses, TasksType} from '../../store/tasks/tasks-types';
-import {FiltersType} from '../../store/todolist/todolist-types';
-import {removeTaskThunk, updateTaskThunk} from '../../store/tasks/tasks-reducer';
-import {filterSelector} from '../../store/tasks/tasks-selectors';
+import React, { ChangeEvent } from "react";
+import { TaskItem } from "./TaskItem";
+import { useAppDispatch, useAppSelector } from "../../store";
+import { TaskStatuses, TasksType } from "../../store/tasks/tasks-types";
+import { FiltersType } from "../../store/todolist/todolist-types";
+import { removeTaskTC, updateTaskTC } from "../../store/tasks/tasks-reducer";
+import { filterSelector } from "../../store/tasks/tasks-selectors";
 
 interface TaskListType {
-    filter: FiltersType
-    todolistId: string
+  filter: FiltersType;
+  todolistId: string;
 }
 
-export const TaskList: React.FC<TaskListType> = ({
-                                                     filter,
-                                                     todolistId,
-                                                 }) => {
-    const dispatch = useAppDispatch();
-    const tasks = useAppSelector<TasksType[] | undefined>(state => filterSelector(state, filter, todolistId))
+export const TaskList: React.FC<TaskListType> = ({ filter, todolistId }) => {
+  const dispatch = useAppDispatch();
+  const tasks = useAppSelector<TasksType[] | undefined>((state) =>
+    filterSelector(state, filter, todolistId)
+  );
 
-    const updateStatusCheckbox = (e: ChangeEvent<HTMLInputElement>, taskID: string) => {
-        const status = e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New
-        dispatch(updateTaskThunk(todolistId, taskID, {status}))
-    }
+  const updateStatusCheckbox = (
+    e: ChangeEvent<HTMLInputElement>,
+    taskID: string
+  ): void => {
+    const status = e.currentTarget.checked
+      ? TaskStatuses.Completed
+      : TaskStatuses.New;
+    dispatch(updateTaskTC(todolistId, taskID, { status }));
+  };
 
-    const handlerRemoveTask = (taskID: string) => dispatch(removeTaskThunk(taskID, todolistId))
-    const onChangeValueTextSpan = (newValue: string, taskID: string) => dispatch(updateTaskThunk(todolistId, taskID, {title: newValue}))
+  const handlerRemoveTask = (taskID: string): void =>
+    dispatch(removeTaskTC(taskID, todolistId));
+  const onChangeValueTextSpan = (newValue: string, taskID: string): void =>
+    dispatch(updateTaskTC(todolistId, taskID, { title: newValue }));
 
-    return (
-        <>
-            {tasks && tasks.map(task => {
-                return (
-                    <TaskItem key={task.id}
-                              taskTitle={task.title}
-                              taskID={task.id}
-                              taskStatus={task.status}
-                              todolistId={todolistId}
-                              entityStatusTask={task.entityStatusTask}
-                              updateStatusCheckbox={updateStatusCheckbox}
-                              handlerRemoveTask={handlerRemoveTask}
-                              onChangeValueTextSpan={onChangeValueTextSpan}
-                    />
-                )
-            })}
-        </>
-    );
+  const tasksItemLayer =
+    tasks &&
+    tasks.map((task) => {
+      return (
+        <TaskItem
+          key={task.id}
+          taskTitle={task.title}
+          taskID={task.id}
+          taskStatus={task.status}
+          todolistId={todolistId}
+          entityStatusTask={task.entityStatusTask}
+          updateStatusCheckbox={updateStatusCheckbox}
+          handlerRemoveTask={handlerRemoveTask}
+          onChangeValueTextSpan={onChangeValueTextSpan}
+        />
+      );
+    });
+
+  return <>{tasksItemLayer}</>;
 };
-
