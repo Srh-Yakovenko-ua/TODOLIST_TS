cd0f333051a476ba6c67012958d0ceac74742a54
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-01-07 22:54:44 +0300

    Refactor AddItemForm
     - add a check for the maximum number of letters

--- src/common/AddItemForm.tsx
+++ src/common/AddItemForm.tsx
@@ -1,67 +1,71 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
-
-import {IconButton, TextField, Tooltip} from '@mui/material';
-import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
-
+import React, { ChangeEvent, KeyboardEvent, useState } from "react";
+import { IconButton, TextField, Tooltip } from "@mui/material";
+import LibraryAddIcon from "@mui/icons-material/LibraryAdd";
 
 interface AddFormTodoList {
-    addItem: (title: string) => void
-    label?: string
-    title?: string
-    disabled?: boolean
+  addItem: (title: string) => void;
+  label?: string;
+  title?: string;
+  disabled?: boolean;
 }
 
 export const AddItemForm: React.FC<AddFormTodoList> = ({
-                                                           addItem,
-                                                           label,
-                                                           title,
-                                                           disabled,
-                                                       }) => {
-    const [value, setValue] = useState<string>('');
-    const [error, setError] = useState<string>('');
+  addItem,
+  label,
+  title,
+  disabled,
+}) => {
+  const [value, setValue] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
-
-    const onChangeHandlerValue = (e: ChangeEvent<HTMLInputElement>) => {
-        setError('')
-        setValue(e.currentTarget.value)
+  const onChangeHandlerValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    if (value?.trim()?.length > 100) {
+      setError("must be less than 100 characters");
+    } else {
+      setError("");
+      setValue(value);
     }
-    const onKeyUpHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            addItemHandler()
-        }
+  };
+  const onKeyUpHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (value?.trim()?.length > 100) return;
+
+    if (e.key === "Enter") addItemHandler();
+  };
+  const addItemHandler = () => {
+    if (value?.trim()?.length > 100) {
+      return;
     }
-    const addItemHandler = () => {
-        if (value.trim() !== '') {
-            addItem(value)
-            setValue('')
-            setError('')
-        } else setError('empty field add task')
-    }
+    if (value?.trim() !== "") {
+      addItem(value);
+      setValue("");
+      setError("");
+    } else setError("empty field add task");
+  };
 
+  return (
+    <>
+      <TextField
+        type="text"
+        label={!label ? "create a new task" : label}
+        helperText={error ? error : "print text..."}
+        error={!!error}
+        variant="standard"
+        value={value}
+        onChange={onChangeHandlerValue}
+        onKeyUp={onKeyUpHandler}
+        disabled={disabled}
+      />
 
-    return (
-        <>
-            <TextField type="text"
-                       label={!label ? 'create a new task' : label}
-                       helperText={error ? error : 'print text...'}
-                       error={!!error}
-                       variant="standard"
-                       value={value}
-                       onChange={onChangeHandlerValue}
-                       onKeyUp={onKeyUpHandler}
-                       disabled={disabled}
-
-            />
-            <Tooltip title={!title ? 'add Task' : title}
-                     placement="right">
-                <IconButton onClick={addItemHandler}
-                            disabled={disabled}
-                            color="primary">
-                    <LibraryAddIcon/>
-                </IconButton>
-            </Tooltip>
-
-        </>
-    );
+      <Tooltip title={!title ? "add Task" : title} placement="right">
+        <IconButton
+          onClick={addItemHandler}
+          disabled={disabled}
+          color="primary"
+        >
+          <LibraryAddIcon />
+        </IconButton>
+      </Tooltip>
+    </>
+  );
 };
-
