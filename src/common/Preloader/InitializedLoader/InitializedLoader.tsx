929d708053ff2fc8f6d008890763bcbdba944a46
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-21 00:56:32 +0300

    feat(initializedLoader): loader for the entire application

---
+++ src/common/Preloader/InitializedLoader/InitializedLoader.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, CircularProgress } from "@mui/material";
+
+const initializedLoaderContainer = {
+  display: "flex",
+  width: "100%",
+  height: "100vh",
+  alignItems: "center",
+  justifyContent: "center",
+  background: "#f0f8ff",
+};
+export const InitializedLoader = () => {
+  return (
+    <Box sx={initializedLoaderContainer}>
+      <CircularProgress />
+    </Box>
+  );
+};
