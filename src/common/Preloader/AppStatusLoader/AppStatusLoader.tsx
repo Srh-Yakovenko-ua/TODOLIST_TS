7c8e37d8700c2b9d40e98e0720255fab598671a2
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-21 00:58:13 +0300

    refactor(Loader): change name
                              changed positioning

---
+++ src/common/Preloader/AppStatusLoader/AppStatusLoader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, LinearProgress } from "@mui/material";
+
+const appStatusLoaderContainer = { position: "absolute", width: "100%" };
+
+export const AppStatusLoader = () => {
+  return (
+    <Box sx={appStatusLoaderContainer}>
+      <LinearProgress color="primary" />
+    </Box>
+  );
+};
--- src/common/Preloader/preloader-style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const WrapperAppPreloader = styled.div`
-  position: fixed;
-  top: 30%;
-  text-align: center;
-  width: 100%;
-`;
