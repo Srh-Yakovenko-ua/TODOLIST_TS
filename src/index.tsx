d2ba6c95f54ac6c31ce1678e501e3211a6c33a46
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-03-04 21:37:21 +0300

    refactor(index): change browser to hash

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { store } from './store'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 )
 
